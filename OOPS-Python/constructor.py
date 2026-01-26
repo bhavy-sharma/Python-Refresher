2df5505bc63ae0b9b1e23266a2a8400487904bee
bhavy-sharma <bhavymay18@gmail.com>
2026-01-26 12:48:48 +0530

    constructor optimize

--- OOPS-Python/constructor.py
+++ OOPS-Python/constructor.py
@@ -1,9 +1,9 @@
 class Student:
-    def __init__(self):
+    def __init__(self,fullname):
+        self.name = fullname
         print("Adding New Student in DB")
-    name = "Bhavy Sharma"
 
-s1 = Student()
+s1 = Student("Bhavy Sharma")
 print(s1.name)
-s2 = Student()
+s2 = Student("Coder Shab")
 print(s2.name)
