20c73dcb27e3421f9e99f59e7c184fe34e2bb41e
bhavy-sharma <bhavymay18@gmail.com>
2026-01-28 21:57:42 +0530

    2D array

--- Numpy/array-dimentions.py
+++ Numpy/array-dimentions.py
@@ -2,7 +2,10 @@ import numpy as np
 
 arr = np.array(42)    # 0-D array
 
-arr1 = np.array([1,2,3,4,5])
+arr1 = np.array([1,2,3,4,5])  # 1D array
+
+arr2 = np.array([[1,2,3], [1,2,3]])  # 2D array
 
 print(arr)
 print(arr1)
+print(arr2)
