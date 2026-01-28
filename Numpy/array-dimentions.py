892e027dce70cef8237e4010d2c8337f191624ce
bhavy-sharma <bhavymay18@gmail.com>
2026-01-28 22:01:20 +0530

    3D array done

--- Numpy/array-dimentions.py
+++ Numpy/array-dimentions.py
@@ -6,6 +6,9 @@ arr1 = np.array([1,2,3,4,5])  # 1D array
 
 arr2 = np.array([[1,2,3], [1,2,3]])  # 2D array
 
+arr3 = np.array([[[1,2,3], [4,5,6]], [[1,2,3], [4,5,6]]])  # 3D array
+
 print(arr)
 print(arr1)
 print(arr2)
+print(arr3)
