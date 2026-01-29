61fa002388f7732b6c45d2f9e81fc81c3ce74368
bhavy-sharma <bhavymay18@gmail.com>
2026-01-30 05:21:10 +0530

    Starting to Given index Slicing

--- Numpy/slicing-arrays.py
+++ Numpy/slicing-arrays.py
@@ -2,4 +2,9 @@ import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5, 6, 7])
 
-print(arr[1:])   # Start and the end 
+# print(arr[1:5])   # Start and the end 
+
+print(arr[:5])   # From Starting to the given index 
+
+
+# print(arr[1:5])   # Start and the end 
