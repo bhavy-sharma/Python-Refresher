25d8087f6a2faaa1ae990204143c61cc3c72965f
bhavy-sharma <bhavymay18@gmail.com>
2026-01-30 05:27:42 +0530

    Slicing in 2D array

--- Numpy/slicing-arrays.py
+++ Numpy/slicing-arrays.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
+arr = np.array([[1, 2, 3, 4, 5],
+                 [6, 7, 8, 9, 10]])
+
 
 # print(arr[1:5])   # Start and the end 
 
@@ -8,4 +10,14 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7])
 
 # print(arr[2:])   # Form given index to the end of the array 
 
-print(arr[1:5:2])   # From 2nd element to the 4th element with step of 2
+# print(arr[1:5:2])   # From 2nd element to the 4th element with step of 2
+
+# print(arr[0:2, 2])
+
+print(arr[0:2, 1:4])
+
+
+
+
+
+
