f1f0fd54dfe0ad6777e0cbf819b85ccda7a6aa40
bhavy-sharma <bhavymay18@gmail.com>
2026-01-30 05:05:26 +0530

    Make coustmize array dimension

--- Numpy/array-dimentions.py
+++ Numpy/array-dimentions.py
@@ -1,14 +1,20 @@
 import numpy as np
 
-arr = np.array(42)    # 0-D array
+# arr = np.array(42)    # 0-D array
 
-arr1 = np.array([1,2,3,4,5])  # 1D array
+# arr1 = np.array([1,2,3,4,5])  # 1D array
 
-arr2 = np.array([[1,2,3], [1,2,3]])  # 2D array
+# arr2 = np.array([[1,2,3], [1,2,3]])  # 2D array
 
-arr3 = np.array([[[1,2,3], [4,5,6]], [[1,2,3], [4,5,6]]])  # 3D array
+# arr3 = np.array([[[1,2,3], [4,5,6]], [[1,2,3], [4,5,6]]])  # 3D array
 
-print(arr.ndim)
-print(arr1.ndim)
-print(arr2.ndim)
-print(arr3.ndim)
+# print(arr.ndim)
+# print(arr1.ndim)
+# print(arr2.ndim)
+# print(arr3.ndim)
+
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+
+print(arr)
+print("Number of DImension is ", arr.ndim)
