7b1298adac18ad67d0d0d27ff2ed3a1a08a459bb
bhavy-sharma <bhavymay18@gmail.com>
2026-01-30 05:18:05 +0530

    Accessing the 3d array logic

--- Numpy/access-array-elements.py
+++ Numpy/access-array-elements.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
+arr = np.array([[[1, 2, 3],
+                  [4, 5, 6]],
+                    [[7, 8, 9],
+                      [10, 11, 12]]])
 
-print(arr[3] + arr[4])
+
+print("The 3rd element in second row : ", arr[1,1,1])   #11
