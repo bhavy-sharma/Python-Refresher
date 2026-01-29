2ef63940faf1bf27f5a7a3d55fec26d32cb3e481
bhavy-sharma <bhavymay18@gmail.com>
2026-01-30 05:24:22 +0530

    From 2nd element to the 4th element with step of 2

--- Numpy/slicing-arrays.py
+++ Numpy/slicing-arrays.py
@@ -6,4 +6,6 @@ arr = np.array([1, 2, 3, 4, 5, 6, 7])
 
 # print(arr[:5])   # From Starting to the given index 
 
-print(arr[2:])   # Form given index to the end of the array 
+# print(arr[2:])   # Form given index to the end of the array 
+
+print(arr[1:5:2])   # From 2nd element to the 4th element with step of 2
